1b2426bd84bbdbe81c6a128229168484cf04a30a
Neema <patelneema93@gmail.com>
2022-02-08 21:52:55 -0500

    fetch data in list

--- to-do/src/components/Input.jsx
+++ to-do/src/components/Input.jsx
@@ -1,8 +1,10 @@
 import React, { useState,useEffect } from "react";
 import axios from 'axios';
-import './input.css'
+import './input.css';
+
   export default function Input() {
     const [name, setName] = useState("");
+   
     function handleSubmit(e) {
         e.preventDefault();
         const user = {
@@ -11,27 +13,29 @@ import './input.css'
         axios.post("http://localhost:3001/api/todo", user)
             .then(res => {
                 console.log(res);
-                // history.push("/List")
-
+                
             })
             .catch(err => {
                 console.log(err)
             });
             handleReset();
             setTimeout(()=> {
-                // history.push('/List');
+               
                 },1500);
             // alert.show(‘Thanks for Donating’);
     }
     function handleReset() {
         setName("");
     }
+    function refreshPage() {
+        window.location.reload(false);
+      }
       return (
           <div>
           <form onSubmit={handleSubmit} >
           <input type="text" value={name}
                         onChange={(event) => setName(event.target.value)} />
-          <button type="submit">Add</button>
+          <button type="submit" onClick={refreshPage}>Add</button>
           </form>
           </div>
       );
--- to-do/src/components/List.jsx
+++ to-do/src/components/List.jsx
@@ -1,15 +1,32 @@
+import axios from "axios";
 import React, { useState,useEffect } from "react";
 
 import './list.css'
-  export default function Input() {
+  export default function List() {
+    const [name, setName] = useState([]);
+    useEffect(() => {
+        axios.get(`http://localhost:3001/api/getalltodo`)
+        .then((response) => {
+          console.log("data",response);
+          setName(response.data);
+        })
+          .catch(error => {
+            console.log("Error: " + error);
+            })
+        }, []);
+        const allName = name.map((item) => {
+          console.log("items",item);
+          return <li class="card">{item.name}</li> ;
+        });
+    
       return (
           <div>
         
         <ul class="layout__list">
         
-        <li class="card">Book reading</li>
-        <li class="card">shopping</li>
-       <li class="card">movie</li>
+        {allName}
+        {/* <li class="card">shopping</li>
+       <li class="card">movie</li> */}
         </ul>
           
           </div>
